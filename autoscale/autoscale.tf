02fe9ed88bb177e0010a442c0981fdbf02ae25ee
achalad <achalad@exetel.com.au>
2022-01-05 10:52:23 +0530

    third commit

--- autoscale/autoscale.tf
+++ autoscale/autoscale.tf
@@ -13,7 +13,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgi0ogjGnJno8zou2pk9YuSh5I8KxRFXh4Z0CYrMYph7tmi5Er+pfKOSWblUo/2SolCFgn8p+xg+a6jIac07aWkWi78eajaqIPIZhFxPiWw20v0rjCNdmZB+m9ZOxUNzhsUDkaIxZ+xhKjKNpcxGqqhpLGBOx1ZYAdtG5I+CxX2a+uZV0ixXB+Cd2zCIRg3/lfvsTCx7ERa4rt6pd7ngQZ0fKc6gX7yFnWxPo0qTi64dtD77qeLAUB6n+RYqQnyB6kXn5L+j9L6spK7lfvKoZwtmALAdEVFkGsGw82Bd2yJt61yiocU0TWnY2hYw13IuXg78U+fA3o6w7dFnttfzW7 kuba"
+  public_key = "your pub key"
 }
 
 #aws_launch_configuration
